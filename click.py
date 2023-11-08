0baafa4ab947d28015dc42c94bc666e95effe987
Josh Chen <97403813+joshxgchen@users.noreply.github.com>
2023-11-08 12:03:11 -0800

    Create click.py

---
+++ click.py
@@ -0,0 +1,20 @@
+from random import uniform
+from time import sleep
+
+from keyboard import is_pressed
+from mouse import click
+
+def autoclicker():
+    while True:
+        if is_pressed('esc'):
+            print("Program cancelled")
+            break
+
+        if not is_pressed('a'):
+            continue
+
+        click(button='left');
+        sleep(uniform(0.05, 0.1))
+
+if __name__ == '__main__':
+    autoclicker()
